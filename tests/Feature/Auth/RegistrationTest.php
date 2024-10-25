b3d330dd4d04f0a3070bf7f640961be12d61f860
Brimsky <juruma66@gmail.com>
2024-10-25 10:46:51 +0300

    docekr

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -12,18 +12,19 @@ class RegistrationTest extends TestCase
 
     public function test_registration_screen_can_be_rendered(): void
     {
-        $response = $this->get('/register');
+        $response = $this->get("/register");
 
         $response->assertStatus(200);
     }
 
     public function test_new_users_can_register(): void
     {
-        $response = $this->post('/register', [
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-            'password' => 'password',
-            'password_confirmation' => 'password',
+        $response = $this->post("/register", [
+            "name" => "John",
+            "surname" => "Doe", // Add this line
+            "email" => "john@example.com",
+            "password" => "password",
+            "password_confirmation" => "password",
         ]);
 
         $this->assertAuthenticated();
