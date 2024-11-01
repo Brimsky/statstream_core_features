7212726efc7ffea2e143f034b049427735f254bd
Brimsky <juruma66@gmail.com>
2024-11-01 02:35:20 +0200

    welcome page

--- resources/js/Components/Home/About.vue
+++ resources/js/Components/Home/About.vue
@@ -1,138 +1,247 @@
 <template>
-  <section class="dark:bg-neutral-900 py-96  container mx-auto antialiased ">
-      <div class="container mx-auto text-black">
-        <h1 class="font-inter font-bold text-5xl md:text-6xl mb-4 text-wrap dark:text-white">CHANGE THE WAY YOU ANALIZE</h1>
-        <h1 class="font-inter font-bold text-5xl md:text-6xl mb-4 text-wrap bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block text-transparent bg-clip-text ">TIMBER</h1>
-        <p class="font-inter mb-8 text-2xl w-1/2 dark:text-white">Transform your analysis with comprehensive timber price statistics, redefining how you perceive market trends.</p>
-        <button class="dark:bg-neutral-700 bg-white dark:text-white text-gray-800 px-6 py-3 rounded-lg font-semibold dark:hover:bg-purple-500 hover:bg-blue-500 hover:text-white transition duration-300">Start Now</button>
-      </div>
+    <!-- Hero Section - Reduced top padding and adjusted height -->
+    <section
+        class="dark:bg-neutral-900 min-h-[85vh] flex items-center antialiased"
+    >
+        <div class="container mx-auto px-4 sm:px-6 lg:px-8 py-8 md:py-12">
+            <div class="max-w-4xl space-y-4">
+                <h1
+                    class="font-inter font-bold text-4xl sm:text-5xl lg:text-6xl text-wrap dark:text-white"
+                >
+                    CHANGE THE WAY YOU ANALIZE
+                </h1>
+                <h1
+                    class="font-inter font-bold text-4xl sm:text-5xl lg:text-6xl text-wrap bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block text-transparent bg-clip-text"
+                >
+                    TIMBER
+                </h1>
+                <p
+                    class="font-inter text-lg sm:text-xl lg:text-2xl w-full md:w-2/3 lg:w-1/2 dark:text-white"
+                >
+                    Transform your analysis with comprehensive timber price
+                    statistics, redefining how you perceive market trends.
+                </p>
+                <button
+                    class="w-full sm:w-auto dark:bg-neutral-700 bg-white dark:text-white text-gray-800 px-6 py-3 rounded-lg font-semibold dark:hover:bg-purple-500 hover:bg-blue-500 hover:text-white transition duration-300"
+                >
+                    Start Now
+                </button>
+            </div>
+        </div>
     </section>
+
+    <!-- Stats Section - Reduced vertical padding -->
     <section class="bg-white dark:bg-neutral-900 rounded-2xl">
-  <div class="mx-auto max-w-screen-xl px-4 py-14 sm:px-6 md:py-16 lg:px-8 dark:bg-neutral-900 shadow-2xl rounded-2xl">
-    <div class="mx-auto max-w-3xl text-center">
-      <h2 class="text-3xl font-bold text-gray-900 dark:text-white sm:text-4xl">Trusted by STATSTREAM</h2>
+        <div
+            class="mx-auto max-w-screen-xl px-4 py-6 sm:py-10 dark:bg-neutral-900 shadow-2xl rounded-2xl"
+        >
+            <div class="mx-auto max-w-3xl text-center px-4 mb-8">
+                <h2
+                    class="text-2xl sm:text-3xl lg:text-4xl font-bold text-gray-900 dark:text-white mb-4"
+                >
+                    Trusted by STATSTREAM
+                </h2>
+                <p
+                    class="text-base sm:text-lg lg:text-xl text-gray-500 dark:text-gray-300"
+                >
+                    Maximize your timber trade with our cutting-edge analytics
+                    and market trends. Understand the ebbs and flows of wood
+                    supply and demand with intuitive graphs and detailed
+                    analytics. Our web app provides real-time data to help you
+                    make informed decisions and stay ahead of the competition.
+                </p>
+            </div>
 
-      <p class="mt-4 text-gray-500 dark:text-gray-300 sm:text-xl">
-        Maximize your timber trade with our cutting-edge analytics and market trends. Understand the ebbs and flows of wood supply and demand with intuitive graphs and detailed analytics. Our web app provides real-time data to help you make informed decisions and stay ahead of the competition.
-      </p>
-    </div>
+            <div class="grid grid-cols-1 gap-4 sm:grid-cols-3">
+                <div
+                    class="flex flex-col rounded-lg bg-blue-50 px-4 py-5 text-center"
+                >
+                    <dd
+                        class="text-3xl sm:text-4xl lg:text-5xl font-extrabold text-blue-600 dark:text-purple-600"
+                    >
+                        $17.3m
+                    </dd>
+                    <dt
+                        class="order-last text-base sm:text-lg font-medium text-gray-500 mt-2"
+                    >
+                        Total Timber Sales in industrie
+                    </dt>
+                </div>
 
-    <div class="mt-8 sm:mt-12">
-      <dl class="grid grid-cols-1 gap-4 sm:grid-cols-3">
-        <div class="flex flex-col rounded-lg bg-blue-50 px-4 py-8 text-center">
-          <dt class="order-last text-lg font-medium text-gray-500">Total Timber Sales in industrie</dt>
+                <div
+                    class="flex flex-col rounded-lg bg-blue-50 px-4 py-5 text-center"
+                >
+                    <dd
+                        class="text-3xl sm:text-4xl lg:text-5xl font-extrabold text-blue-600 dark:text-purple-600"
+                    >
+                        18
+                    </dd>
+                    <dt
+                        class="order-last text-base sm:text-lg font-medium text-gray-500 mt-2"
+                    >
+                        Custom Reports
+                    </dt>
+                </div>
 
-          <dd class="text-4xl font-extrabold text-blue-600  dark:text-purple-600 md:text-5xl">$17.3m</dd>
+                <div
+                    class="flex flex-col rounded-lg bg-blue-50 px-4 py-5 text-center"
+                >
+                    <dd
+                        class="text-3xl sm:text-4xl lg:text-5xl font-extrabold text-blue-600 dark:text-purple-600"
+                    >
+                        37
+                    </dd>
+                    <dt
+                        class="order-last text-base sm:text-lg font-medium text-gray-500 mt-2"
+                    >
+                        Timber Varieties
+                    </dt>
+                </div>
+            </div>
         </div>
+    </section>
 
-        <div class="flex flex-col rounded-lg bg-blue-50 px-4 py-8 text-center">
-          <dt class="order-last text-lg font-medium text-gray-500">Custom Reports</dt>
+    <!-- Features Section - Adjusted spacing -->
+    <section class="dark:bg-neutral-900 bg-white text-black dark:text-white">
+        <div class="mx-auto max-w-screen-xl px-4 py-8">
+            <div class="mx-auto max-w-lg text-center mb-8">
+                <h2 class="text-2xl sm:text-3xl lg:text-4xl font-bold mb-4">
+                    Enhance Your Forestry Operations
+                </h2>
+                <p class="text-base sm:text-lg text-black dark:text-gray-300">
+                    Deploy targeted marketing strategies with our digital
+                    toolkit designed specifically for the timber industry.
+                    Elevate your brand and connect with more customers through
+                    tailored digital campaigns.
+                </p>
+            </div>
 
-          <dd class="text-4xl font-extrabold text-blue-600 dark:text-purple-600 md:text-5xl">18</dd>
+            <div
+                class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 sm:gap-6 mb-8"
+            >
+                <a
+                    class="block rounded-xl border border-white dark:border-gray-800 p-6 shadow-xl transition hover:border-blue-600 hover:shadow-blue-600/50 dark:hover:border-purple-600 dark:hover:shadow-purple-600/50"
+                    href="#"
+                >
+                    <span
+                        class="text-3xl sm:text-4xl flex justify-center items-center"
+                        >🌲</span
+                    >
+                    <h2
+                        class="mt-4 text-lg sm:text-xl font-bold text-black dark:text-gray-300"
+                    >
+                        Sustainable Growth Strategies
+                    </h2>
+                    <p class="mt-2 text-sm text-black dark:text-gray-300">
+                        Learn to market your eco-friendly practices effectively
+                        to reach a green-conscious audience, amplifying your
+                        commitment to sustainability.
+                    </p>
+                </a>
+
+                <a
+                    class="block rounded-xl border border-white dark:border-gray-800 p-6 shadow-xl transition hover:border-blue-600 hover:shadow-blue-600/50 dark:hover:border-purple-600 dark:hover:shadow-purple-600/50"
+                    href="#"
+                >
+                    <span
+                        class="text-3xl sm:text-4xl flex justify-center items-center"
+                        >🔍</span
+                    >
+                    <h2
+                        class="mt-4 text-lg sm:text-xl font-bold text-black dark:text-gray-300"
+                    >
+                        Market Trend Analysis
+                    </h2>
+                    <p class="mt-2 text-sm text-black dark:text-gray-300">
+                        Gain insights into the latest market trends, allowing
+                        you to adjust your strategies in real-time and stay
+                        ahead of industry shifts.
+                    </p>
+                </a>
+
+                <a
+                    class="block rounded-xl border border-white dark:border-gray-800 p-6 shadow-xl transition hover:border-blue-600 hover:shadow-blue-600/50 dark:hover:border-purple-600 dark:hover:shadow-purple-600/50"
+                    href="#"
+                >
+                    <span
+                        class="text-3xl sm:text-4xl flex justify-center items-center"
+                        >📊</span
+                    >
+                    <h2
+                        class="mt-4 text-lg sm:text-xl font-bold text-black dark:text-gray-300"
+                    >
+                        Data-Driven Sales Techniques
+                    </h2>
+                    <p class="mt-2 text-sm text-black dark:text-gray-300">
+                        Harness the power of analytics to refine your sales
+                        approach, targeting the right markets with the right
+                        products at the right time.
+                    </p>
+                </a>
+
+                <a
+                    class="block rounded-xl border border-white dark:border-gray-800 p-6 shadow-xl transition hover:border-blue-600 hover:shadow-blue-600/50 dark:hover:border-purple-600 dark:hover:shadow-purple-600/50"
+                    href="#"
+                >
+                    <span
+                        class="text-3xl sm:text-4xl flex justify-center items-center"
+                        >🤝</span
+                    >
+                    <h2
+                        class="mt-4 text-lg sm:text-xl font-bold text-black dark:text-gray-300"
+                    >
+                        Partnership Opportunities
+                    </h2>
+                    <p class="mt-2 text-sm text-black dark:text-gray-300">
+                        Discover potential partnership avenues to expand your
+                        reach, including collaborations with environmental
+                        organizations and industry leaders.
+                    </p>
+                </a>
+
+                <a
+                    class="block rounded-xl border border-white dark:border-gray-800 p-6 shadow-xl transition hover:border-blue-600 hover:shadow-blue-600/50 dark:hover:border-purple-600 dark:hover:shadow-purple-600/50"
+                    href="#"
+                >
+                    <span
+                        class="text-3xl sm:text-4xl flex justify-center items-center"
+                        >🛠</span
+                    >
+                    <h2
+                        class="mt-4 text-lg sm:text-xl font-bold text-black dark:text-gray-300"
+                    >
+                        Customizable Marketing Tools
+                    </h2>
+                    <p class="mt-2 text-sm text-black dark:text-gray-300">
+                        Utilize customizable digital marketing tools that cater
+                        to the unique aspects of the timber industry, enhancing
+                        your operational efficiency.
+                    </p>
+                </a>
+            </div>
+
+            <div class="text-center">
+                <a
+                    href="#"
+                    class="inline-block w-full sm:w-auto rounded dark:bg-purple-600 bg-blue-600 px-8 sm:px-12 py-3 text-sm font-medium text-white transition dark:hover:bg-purple-800 hover:bg-blue-800 focus:outline-none focus:ring focus:ring-yellow-400"
+                >
+                    Get Started Today
+                </a>
+            </div>
         </div>
-
-        <div class="flex flex-col rounded-lg bg-blue-50 px-4 py-8 text-center">
-          <dt class="order-last text-lg font-medium text-gray-500">Timber Varieties</dt>
-
-          <dd class="text-4xl font-extrabold text-blue-600 dark:text-purple-600 md:text-5xl">37</dd>
-        </div>
-      </dl>
-    </div>
-  </div>
-</section>
-  <section class="dark:bg-neutral-900 bg-white text-black dark:text-white">
-    <div class="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
-      <div class="mx-auto max-w-lg text-center">
-        <h2 class="text-3xl font-bold sm:text-4xl">Enhance Your Forestry Operations</h2>
-  
-        <p class="mt-4 text-black dark:text-gray-300">
-          Deploy targeted marketing strategies with our digital toolkit designed specifically for the timber industry. Elevate your brand and connect with more customers through tailored digital campaigns.
-        </p>
-      </div>
-  
-      <div class="mt-8 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
-        <a class="block rounded-xl border border-white dark:border-gray-800 p-8 shadow-xl transition hover:border-blue-600 hover:shadow-blue-600/50 dark:hover:border-purple-600 dark:hover:shadow-purple-600/50"
-          href="#">
-          
-          <span class="text-4xl flex justify-center items-center">🌲</span>
-  
-          <h2 class="mt-4 text-xl font-bold text-black dark:text-gray-300">Sustainable Growth Strategies</h2>
-  
-          <p class="mt-1 text-sm text-black dark:text-gray-300">
-            Learn to market your eco-friendly practices effectively to reach a green-conscious audience, amplifying your commitment to sustainability.
-          </p>
-        </a>
-        <a
-          class="block rounded-xl border border-white dark:border-gray-800 p-8 shadow-xl transition hover:border-blue-600 hover:shadow-blue-600/50 dark:hover:border-purple-600 dark:hover:shadow-purple-600/50"
-          href="#"
-        >
-          <span class="text-4xl flex justify-center items-center">🔍</span>
-  
-          <h2 class="mt-4 text-xl font-bold text-black dark:text-gray-300">Market Trend Analysis</h2>
-  
-          <p class="mt-1 text-sm text-black dark:text-gray-300">
-            Gain insights into the latest market trends, allowing you to adjust your strategies in real-time and stay ahead of industry shifts.
-          </p>
-        </a>
-  
-        <a
-          class="block rounded-xl border border-white dark:border-gray-800 p-8 shadow-xl transition hover:border-blue-600 hover:shadow-blue-600/50 dark:hover:border-purple-600 dark:hover:shadow-purple-600/50"
-          href="#"
-        >
-          <span class="text-4xl flex justify-center items-center">📊</span>
-  
-          <h2 class="mt-4 text-xl font-bold text-black dark:text-gray-300">Data-Driven Sales Techniques</h2>
-  
-          <p class="mt-1 text-sm text-black dark:text-gray-300">
-            Harness the power of analytics to refine your sales approach, targeting the right markets with the right products at the right time.
-          </p>
-        </a>
-  
-        <a
-          class="block rounded-xl border border-white dark:border-gray-800 p-8 shadow-xl transition hover:border-blue-600 hover:shadow-blue-600/50 dark:hover:border-purple-600 dark:hover:shadow-purple-600/50"
-          href="#"
-        >
-          <span class="text-4xl flex justify-center items-center">🤝</span>
-  
-          <h2 class="mt-4 text-xl font-bold text-black dark:text-gray-300">Partnership Opportunities</h2>
-  
-          <p class="mt-1 text-sm text-black dark:text-gray-300">
-            Discover potential partnership avenues to expand your reach, including collaborations with environmental organizations and industry leaders.
-          </p>
-        </a>
-  
-        <a
-          class="block rounded-xl border border-white dark:border-gray-800 p-8 shadow-xl transition hover:border-blue-600 hover:shadow-blue-600/50 dark:hover:border-purple-600 dark:hover:shadow-purple-600/50"
-          href="#"
-        >
-        <span class="text-4xl flex justify-center items-center">🛠</span>
-  
-          <h2 class="mt-4 text-xl font-bold text-black dark:text-gray-300">Customizable Marketing Tools</h2>
-  
-          <p class="mt-1 text-sm text-black dark:text-gray-300">
-            Utilize customizable digital marketing tools that cater to the unique aspects of the timber industry, enhancing your operational efficiency.
-          </p>
-        </a>      
-      </div>
-  
-      <div class="mt-12 text-center">
-        <a
-          href="#"
-          class="inline-block rounded dark:bg-purple-600  bg-blue-600 px-12 py-3 text-sm font-medium text-white transition dark:hover:bg-purple-800 hover:bg-blue-800 focus:outline-none focus:ring focus:ring-yellow-400"
-        >
-          Get Started Today
-        </a>
-      </div>
-    </div>
-  </section>
+    </section>
 </template>
 
 <script>
 export default {
-  name: 'Start',
-}
+    name: "Start",
+};
 </script>
 
 <style>
-
+@media (max-width: 640px) {
+    .text-wrap {
+        word-break: break-word;
+    }
+}
 </style>
